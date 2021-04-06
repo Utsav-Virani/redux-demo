0c627c525e99d68e0c0f5ed63033d3038194d50c
Utsav Virani <uvvirani004@gmail.com>
2021-04-06 11:05:13 +0530

    logger middleware added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
-import { createStore } from "redux";
+import { createStore ,applyMiddleware} from "redux";
 import cakeReducer from "./cake/cakeReducer";
 import { combineReducers } from "redux";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
+import logger from 'redux-logger';
 
 const rootReducer = combineReducers({
     cake: cakeReducer,
     iceCream:iceCreamReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(logger));
 
 export default store;
